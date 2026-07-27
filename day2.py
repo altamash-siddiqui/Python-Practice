f0bc91a10effd5ec9d1386772e2452c7ae8b6b41
Altamash Siddiqui <siddiquialtamash08@gmail.com>
2026-07-27 21:04:26 +0530

    Day 2: Function practice

--- day2.py
+++ day2.py
@@ -2,3 +2,24 @@ def welcome():
     print("Welcome to Python")
     print("I am learning Functions")
 welcome()
+
+def greet(name):
+    print("Hello ", name)
+greet("Altamash")
+greet("Sami")
+greet("Python")
+
+def add(a, b):
+    print("Sum =", a + b)
+add(10, 20)
+add(100, 200)
+
+def square(num):
+    print("Square =", num * num)
+square(5)
+square(10)
+
+def multiply(num1, num2):
+    print("Product =", num1 * num2)
+multiply(5, 10)
+multiply(20, 30)
