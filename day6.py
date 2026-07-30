61bea7f771c1b82a2adafab56cfacbf719ebe36f
Altamash Siddiqui <siddiquialtamash08@gmail.com>
2026-07-30 18:38:24 +0530

    Day 6: Added Python Lists Basics

---
+++ day6.py
@@ -0,0 +1,27 @@
+# ---------- Question 1 ----------
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print(fruits)
+
+
+# ---------- Question 2 ----------
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+
+
+# ---------- Question 3 ----------
+
+fruits[1] = "Grapes"
+
+print(fruits)
+
+
+# ---------- Question 4 ----------
+
+fruits.append("Pineapple")
+
+print(fruits)
