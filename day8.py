38b03042f25e75ff1d1126c1d4acea2acbe8e546
Altamash Siddiqui <siddiquialtamash08@gmail.com>
2026-07-31 21:50:39 +0530

    Day 8: Learned Python Tuples

---
+++ day8.py
@@ -0,0 +1,35 @@
+# ---------- Question 1 ----------
+
+fruits = ("Apple", "Banana", "Mango", "Orange")
+
+print(fruits)
+
+
+# ---------- Question 2 ----------
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+
+
+# ---------- Question 3 ----------
+
+#fruits = ("Apple", "Banana", "Mango")
+
+#fruits[1] = "Orange"
+
+#print(fruits)
+
+
+# ---------- Question 4 ----------
+
+fruits = ("Apple", "Banana", "Mango", "Orange")
+
+print("Total Fruits =", len(fruits))
+
+
+# ---------- Question 5 ----------
+
+for fruit in fruits:
+    print(fruit)
